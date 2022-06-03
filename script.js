bbde64da6ebb8ac54ea2cfcff7fbf93200e26ac2
imrajat24 <imrajatgulati@gmail.com>
2022-06-03 12:19:36 +0530

    addded the image on 3rd june

---
+++ script.js
@@ -0,0 +1,27 @@
+// external js: flickity.pkgd.js
+
+var autoScrollDelay = 4000;
+
+var flkty = new Flickity('#carousel1', {
+  autoPlay: autoScrollDelay
+});
+
+var flkty = new Flickity('#carousel2', {
+  autoPlay: autoScrollDelay
+});
+
+var flkty = new Flickity('#carousel3', {
+  autoPlay: autoScrollDelay
+});
+var flkty = new Flickity('#carousel4', {
+  autoPlay: autoScrollDelay
+});
+var flkty = new Flickity('#carousel5', {
+  autoPlay: autoScrollDelay
+});
+var flkty = new Flickity('#carousel6', {
+  autoPlay: autoScrollDelay
+});
+var flkty = new Flickity('#carousel7', {
+  autoPlay: autoScrollDelay
+});
